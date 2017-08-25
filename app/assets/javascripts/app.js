aacc28ce9fee68ae95445282d710089468f26f58
Satheesh <sbejjanki@sofi.org>
2017-08-25 17:44:24 -0600

    add Number format in Jquery

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,10 +3,26 @@ $( document ).ready(function() {
 	          function(data) {
 	              // data is a JSON
 		  			var table = $('table');
-		  			table.append("<tr><th>Year</th><th>Balance(Wells)</th><th>Balance(SOFI)</th></tr>");
+		  			var well = 0.0;
+		  			var sofi = 0.0;
+		  			var wellsTotal = 0.0;
+		  			var sofiTotal = 0.0;
+		  			var diff = 0.0;
+		  			table.append("<tr><th>Year</th><th>Balance(Wells)</th><th>Balance(SOFI)</th><</tr>");
 		  			 $.each(data, function(key, val) {
-		  			table.append("<tr><td>" + val.year+ "</td><td> $" + val.wellsBalance+ "</td> <td>$" + val.sofiBalance+ "</td></tr><br />");
+		  				 wellsTotal = wellsTotal + val.wellsBalance;
+		  				 sofiTotal = sofiTotal + val.sofiBalance;
+		  				 wells = val.wellsBalance;
+		  				 wells = wells.toLocaleString('en');
+		  				 sofi = val.sofiBalance;
+		  				 sofi = sofi.toLocaleString('en');
+		  			table.append("<tr><td>" + val.year+ "</td><td> $" + wells + "</td> <td>$" + sofi + "</td></tr><br />");
 		  			 });
+		  			 diff = sofiTotal - wellsTotal;
+
+		  			 diff = diff.toFixed(2);
+
+		  			 table.append("<tr><td>Ended Up with this Amount: </td><td>$" + diff + " </td></tr>");
 		  			$("#results").append(table);
 	       });
 });
